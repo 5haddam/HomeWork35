0623eae3e049d57963626dbc2c4eef30626b3f42
5haddam <131292268+5haddam@users.noreply.github.com>
2023-07-01 11:38:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,3 +9,13 @@ for (let i = 1; i <= 10; i++) {
     }
     table.append(tr);
 }
+
+// const wrapp = document.querySelector('#wrapp');
+// wrapp.classList.add('grid-wrapp');
+
+// for (let i = 1; i <= 100; i++) {
+//     const div = document.createElement('div');
+//     div.innerText = i;
+//     div.classList.add('grid');
+//     wrapp.append(div);
+// }
