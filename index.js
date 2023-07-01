78e865d6f0f6037251eaccb7ab882ae2f4619844
5haddam <131292268+5haddam@users.noreply.github.com>
2023-07-01 11:33:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const wrapp = document.querySelector('#wrapp');
-wrapp.classList.add('grid-wrapp');
+const table = document.querySelector('#table');
 
-for (let i = 1; i <= 100; i++) {
-    const div = document.createElement('div');
-    div.innerText = i;
-    div.classList.add('grid');
-    wrapp.append(div);
+for (let i = 1; i <= 10; i++) {
+    const tr = document.createElement('tr');
+    for (let j = i * 10 - 9; j <= i * 10; j++) {
+        const td = document.createElement('td');
+        td.innerText = j;
+        tr.append(td);
+    }
+    table.append(tr);
 }
